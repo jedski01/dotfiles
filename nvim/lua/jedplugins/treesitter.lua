4957ecbe48022171438efde0feb68b1f07763fc9
jedski01 <jedikin@gmail.com>
2023-05-18 05:32:33 -0700

    Enable treesitter indentation

--- nvim/lua/jedplugins/treesitter.lua
+++ nvim/lua/jedplugins/treesitter.lua
@@ -5,6 +5,9 @@ require('nvim-treesitter.configs').setup({
   highlight = {
     enable = true
   },
+  indent = {
+    enable = true
+  },
   autotag = {
     enable = true,
     filetypes = { 'html', 'xml' }

a8b4f18582f4e441f8177c9175826b1d33bcbf14
jedski01 <jedikin@gmail.com>
2023-08-04 05:39:44 -0700

    Set tabstop and shiftwidth for c files

--- nvim/lua/core/base.lua
+++ nvim/lua/core/base.lua
@@ -40,3 +40,10 @@ vim.api.nvim_create_autocmd("TermOpen", {
 	end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { "c", "cpp", "h" },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end
+})

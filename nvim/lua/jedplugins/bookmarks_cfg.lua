4207f75f5554d1c040737f1e80650cf6c182c5d5
Jed Iquin <jiquin@fortinet.com>
2025-04-06 11:27:49 -0700

    fix merge conflicts issues

--- nvim/lua/jedplugins/bookmarks_cfg.lua
+++ nvim/lua/jedplugins/bookmarks_cfg.lua
@@ -1,23 +1,4 @@
 return {
-<<<<<<< HEAD
-  "tomasky/bookmarks.nvim",
-  config = function()
-    require('bookmarks').setup {
-      on_attach = function(bufnr)
-        local bm = require "bookmarks"
-        local map = vim.keymap.set
-        map("n","mm",bm.bookmark_toggle) -- add or remove bookmark at current line
-        map("n","mi",bm.bookmark_ann) -- add or edit mark annotation at current line
-        map("n","mc",bm.bookmark_clean) -- clean all marks in local buffer
-        map("n","mn",bm.bookmark_next) -- jump to next mark in local buffer
-        map("n","mp",bm.bookmark_prev) -- jump to previous mark in local buffer
-        map("n","ml", function()
-          require("telescope").extensions.bookmarks.list()
-        end) -- show marked file list in quickfix window
-      end
-    }
-  end
-=======
 	"tomasky/bookmarks.nvim",
 	config = function()
 		require("bookmarks").setup({
@@ -41,5 +22,4 @@ return {
 			end,
 		})
 	end,
->>>>>>> d231715 (Update configs)
 }
